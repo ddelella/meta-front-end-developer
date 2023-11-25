bc43307ef268e1a2915426a877f829aac098807a
David Delella <david.delella@sogeti.com>
2023-11-25 15:22:40 -0500

    added more images. split main app into header, nav, main, and footer.

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header id="header">
+      <img src="/images/wide-logo.png" alt="Little Lemon" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+  return (
+    <main id="main">
+      This is my main
+    </main>
+  );
+}
+
+export default Main;
